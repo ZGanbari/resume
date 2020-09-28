c8b31d7424722dc46b6523c51f4f30cd799823ab
ZGhanbari <z.ghanbari14@gmail.com>
2020-09-28 23:06:07 +0330

    Updates

---
+++ precache-manifest.e515ee995a5c45de113ddf29b1f0ddce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a800ea91b92ee0489d1d00ebdc5a148",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "649e494f414865621e64",
+    "url": "/resume/static/css/main.b3ed4e05.chunk.css"
+  },
+  {
+    "revision": "a0871a3a68e3b1a8606a",
+    "url": "/resume/static/js/2.60d65855.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/resume/static/js/2.60d65855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649e494f414865621e64",
+    "url": "/resume/static/js/main.72519e41.chunk.js"
+  },
+  {
+    "revision": "d3d4fd21a642faf061b1",
+    "url": "/resume/static/js/runtime-main.93216231.js"
+  }
+]);
